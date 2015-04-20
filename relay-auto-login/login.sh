df6b1efdd9955a3e3d1ecbfecfff46d288d79bc2
刘新宇 <liuxinyu03@meituan.com>
2015-04-20 12:08:19 +0800

    添加注释

--- relay-auto-login/login.sh
+++ relay-auto-login/login.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 #--------------------------------------------
-# 这是一个自动打ipa的脚本，基于webfrogs的ipa-build书写：https://github.com/webfrogs/xcode_shell/blob/master/ipa-build
-
-# 功能：自动为etao ios app打包，产出物为14个渠道的ipa包
-# 特色：全自动打包，不需要输入任何参数
+# 这是一个自动登陆线上relay的脚本 https://github.com/qq291462491/Learning-code-demo/tree/master/relay-auto-login
+# 功能：直接输入口令即可登陆指定机器
 #--------------------------------------------
 
 ##### 用户配置区 开始 #####
 #
-# 项目根目录，推荐将此脚本放在项目的根目录，这里就不用改了
-# 应用名，确保和Xcode里Product下的target_name.app名字一致
+# 用户名和密码配置
 #
 ##### 用户配置区 结束  #####
 relay_name=liuxinyu03
@@ -30,7 +27,8 @@ then
   		2   ) online_host=dx-hotel-goods01;;  
   		3   ) online_host=dx-hotel-goods02;;  
   		4   ) echo "Punctuation, whitespace, or other";;  
-	esac  
+	esac
+#调用expect脚本  
         ./login.expect $relay_name $relay_password$dynamic_token $online_host  	
    else
        echo -e "\nsorry,too slow" 
@@ -40,11 +38,7 @@ else
     echo -e "\nsorry,too slow"
     exit 0
 fi
-#relay_host=relay.sankuai.com
 
-#relay_name=liuxinyu03
-#relay_password=LIUXy@2013
-echo $dynamic_token
 
 
 
