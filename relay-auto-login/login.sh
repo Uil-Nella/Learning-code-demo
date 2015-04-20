2f1239f49a35d35371c0688252a29c6c39e383b2
刘新宇 <liuxinyu03@meituan.com>
2015-04-20 17:13:25 +0800

    shell final commit

--- relay-auto-login/login.sh
+++ relay-auto-login/login.sh
@@ -16,9 +16,21 @@ echo  '请输入6位的大象口令'
 if read -t 30 -p "dynamic_token:" dynamic_token
 then 
     echo "hello $USER ,请输入对应的编号来登录"
+    echo "###########hotel-goods###############”
     echo "1:dx-hotel-goods-staging01"
     echo "2:dx-hotel-goods01"
     echo "3:dx-hotel-goods02"
+    echo "4:hotel-goods01 lf-online"
+    echo "###########hotel-inventory###############”
+    echo "5:dx-hotel-inventory-staging01"    
+    echo "6:dx-hotel-inventory01"
+    echo "7:dx-hotel-inventory02"
+    echo "8:hotel-inventory01(lf-online)"
+    echo "###########hotel-price###############”
+    echo "9:dx-hotel-price-staging01"
+    echo "10:dx-hotel-price01"
+    echo "11:dx-hotel-price02"
+    echo "12:hotel-price01 lf-online "
     
    if read -t 30 -p "number:" number
    then
@@ -26,7 +38,15 @@ then
   		1   ) online_host=dx-hotel-goods-staging01;;  
   		2   ) online_host=dx-hotel-goods01;;  
   		3   ) online_host=dx-hotel-goods02;;  
-  		4   ) echo "Punctuation, whitespace, or other";;  
+  		4   ) online_host=hotel-goods01;;
+		5   ) online_host=dx-hotel-inventory-staging01;;
+		6   ) online_host=dx-hotel-inventory01;;
+		7   ) online_host=dx-hotel-inventory02;;
+		8   ) online_host=hotel-inventory01;;
+		9   ) online_host=dx-hotel-price-staging01;;
+		10  ) online_host=dx-hotel-price01;;
+		11  ) online_host=dx-hotel-price01;;
+		12  ) online_host=hotel-price01;;  
 	esac
 #调用expect脚本  
         ./login.expect $relay_name $relay_password$dynamic_token $online_host  	
@@ -35,15 +55,9 @@ then
        exit 0
    fi
 else
-    echo -e "\nsorry,too slow"
+    echo -e "\nsorry,too slow ""
+
     exit 0
 fi
-
-
-
-
-
-
-
 
 
